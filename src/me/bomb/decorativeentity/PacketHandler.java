e2bdb63959fad96bf225d479e74d282bda157522
FatSaw <eklesosek@gmail.com>
2024-02-01 04:32:44 +0200

    Update PacketHandler.java

--- src/me/bomb/decorativeentity/PacketHandler.java
+++ src/me/bomb/decorativeentity/PacketHandler.java
@@ -1,6 +1,5 @@
 package me.bomb.decorativeentity;
 
-import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
@@ -8,26 +7,20 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
-import me.bomb.decorativeentity.ArmorstandOptions.ArmorstandOptionsEntry;
-import me.bomb.decorativeentity.MinecartOptions.MinecartOptionsEntry;
-import net.minecraft.server.v1_12_R1.Packet;
 import net.minecraft.server.v1_12_R1.PacketDataSerializer;
 import net.minecraft.server.v1_12_R1.PacketPlayOutMapChunk;
-import net.minecraft.server.v1_12_R1.PlayerConnection;
 
 final class PacketHandler extends ChannelDuplexHandler {
 	
 	private final PacketCache cache;
 	private final Player player;
-	private final PlayerConnection connection;
 	private final PacketDataSerializer packetdataserializer;
 	
 	protected PacketHandler(PacketCache cache, Player player) {
 		this.cache = cache;
 		this.player = player;
-		this.connection = ((CraftPlayer) player).getHandle().playerConnection;
 		this.packetdataserializer = new PacketDataSerializer(Unpooled.buffer(8, 8));
-		connection.networkManager.channel.pipeline().addBefore("packet_handler", "decorativeentity", this);
+		((CraftPlayer) player).getHandle().playerConnection.networkManager.channel.pipeline().addBefore("packet_handler", "decorativeentity", this);
 	}
 	
 	@Override
@@ -45,23 +38,7 @@ final class PacketHandler extends ChannelDuplexHandler {
 				return;
 			} catch (IndexOutOfBoundsException e) { //DO NOT READ FULL CHUNK WE NEED ONLY FIRST 8 BYTES
 			}
-			int x = packetdataserializer.readInt(), z = packetdataserializer.readInt();
-			World world = player.getWorld();
-			String worldname = world.getName();
-			MinecartOptionsEntry[] minecartoptions = cache.minecartoptions.getOptions(worldname, x, z);
-			if (minecartoptions != null) {
-				for(Object p : this.cache.getPackets(world, x, z, minecartoptions)) {
-					connection.sendPacket((Packet<?>) p);
-				}
-			}
-			ArmorstandOptionsEntry[] armorstandoptions = cache.armorstandoptions.getOptions(worldname, x, z);
-			if (armorstandoptions != null) {
-				for(Object p : this.cache.getPackets(world, x, z, armorstandoptions)) {
-					connection.sendPacket((Packet<?>) p);
-				}
-			}
-			
-			
+			cache.sendPacketsForChunk(context, player.getWorld(), packetdataserializer.readInt(), packetdataserializer.readInt());
 		}
 		
 	}
