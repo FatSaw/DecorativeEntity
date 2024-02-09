64f22bfbeabfd6bc66572edf98381d74a59b771b
FatSaw <eklesosek@gmail.com>
2024-02-10 00:13:42 +0200

    Update PacketCache.java

--- src/me/bomb/decorativeentity/PacketCache.java
+++ src/me/bomb/decorativeentity/PacketCache.java
@@ -7,8 +7,17 @@ import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
 
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
 import me.bomb.decorativeentity.ArmorstandOptions.ArmorstandOptionsEntry;
+import me.bomb.decorativeentity.HumanOptions.HumanOptionsEntry;
 import me.bomb.decorativeentity.MinecartOptions.MinecartOptionsEntry;
+import me.bomb.decorativeentity.packet.EnumGamemode;
+import me.bomb.decorativeentity.packet.PacketEncoder;
+import me.bomb.decorativeentity.packet.PacketPlayOutEntityHeadRotation;
+import me.bomb.decorativeentity.packet.PacketPlayOutPlayerSpawn;
+import me.bomb.decorativeentity.packet.PacketPlayOutPlayerInfo;
+import me.bomb.decorativeentity.packet.PacketPlayOutPlayerInfo.EnumPlayerInfoAction;
+import me.bomb.decorativeentity.packet.PacketPlayOutPlayerInfo.PlayerInfoData;
 import net.minecraft.server.v1_12_R1.Block;
 import net.minecraft.server.v1_12_R1.EntityArmorStand;
 import net.minecraft.server.v1_12_R1.PacketPlayOutEntityMetadata;
@@ -19,27 +28,40 @@ final class PacketCache {
 	
 	private final HashMap<MinecartOptionsEntry[],ArrayList<Object>> cacheminecarts = new HashMap<MinecartOptionsEntry[],ArrayList<Object>>();
 	private final HashMap<ArmorstandOptionsEntry[],ArrayList<Object>> cachearmorstands = new HashMap<ArmorstandOptionsEntry[],ArrayList<Object>>();
+	private final HashMap<HumanOptionsEntry[],ArrayList<Object>> cachehumans = new HashMap<HumanOptionsEntry[],ArrayList<Object>>();
 	
 	private final MinecartOptions minecartoptions;
 	private final ArmorstandOptions armorstandoptions;
+	private final HumanOptions humanoptions;
 	
-	protected PacketCache(MinecartOptions minecartoptions, ArmorstandOptions armorstandoptions) {
+	protected PacketCache(MinecartOptions minecartoptions, ArmorstandOptions armorstandoptions, HumanOptions humanoptions) {
 		this.minecartoptions = minecartoptions;
 		this.armorstandoptions = armorstandoptions;
+		this.humanoptions = humanoptions;
 	}
 	
-	protected void sendPacketsForChunk(ChannelHandlerContext context, World world, long chunkpos) {
+	protected void sendPacketsForChunk(ChannelHandlerContext context, ChannelPromise promise, PacketEncoder encoder, World world, long chunkpos) {
 		String worldname = world.getName();
 		MinecartOptionsEntry[] minecartoptions = this.minecartoptions.getOptions(worldname, chunkpos);
 		if (minecartoptions != null) {
 			for(Object p : this.getPackets(world, minecartoptions)) {
-				context.write(p);
+				context.write(p, promise);
 			}
 		}
 		ArmorstandOptionsEntry[] armorstandoptions = this.armorstandoptions.getOptions(worldname, chunkpos);
 		if (armorstandoptions != null) {
 			for(Object p : this.getPackets(world, armorstandoptions)) {
-				context.write(p);
+				context.write(p, promise);
+			}
+		}
+		HumanOptionsEntry[] humanoptions = this.humanoptions.getOptions(worldname, chunkpos);
+		if (humanoptions != null) {
+			for(Object p : this.getPackets(world, humanoptions)) {
+				try {
+					encoder.write(context, p, promise);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
@@ -86,5 +108,27 @@ final class PacketCache {
 			cachearmorstands.put(options, sdandspawns);
 		}
 		return sdandspawns;
+	}
+	private ArrayList<Object> getPackets(World world, HumanOptionsEntry[] options) {
+
+		ArrayList<Object> humanspawns = cachehumans.get(options);
+		if(humanspawns==null) {
+			humanspawns = new ArrayList<Object>();
+			PacketPlayOutPlayerInfo playerinfoadd = new PacketPlayOutPlayerInfo(EnumPlayerInfoAction.ADD_PLAYER), playerinforemove = new PacketPlayOutPlayerInfo(EnumPlayerInfoAction.REMOVE_PLAYER);
+			humanspawns.add(playerinfoadd);
+			for(HumanOptionsEntry option : options) {
+				PlayerInfoData infodata = new PlayerInfoData(option.name, option.skinvalue, option.skinsignature, 0, EnumGamemode.CREATIVE, null);
+				playerinfoadd.playerinfodata.put(option.uuid, infodata);
+				playerinforemove.playerinfodata.put(option.uuid, null);
+				PacketPlayOutPlayerSpawn npcspawnpacket = new PacketPlayOutPlayerSpawn(option.npcid, option.uuid, option.x, option.y, option.z, option.yaw, option.pitch);
+				humanspawns.add(npcspawnpacket);
+				PacketPlayOutEntityHeadRotation rotatepacket = new PacketPlayOutEntityHeadRotation(option.npcid, option.yaw);
+				humanspawns.add(rotatepacket);
+			}
+			humanspawns.add(playerinforemove);
+			cachehumans.put(options, humanspawns);
+		}
+		
+		return humanspawns;
 	}
 }
