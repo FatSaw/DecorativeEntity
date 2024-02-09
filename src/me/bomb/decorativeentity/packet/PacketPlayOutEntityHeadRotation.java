2d8a4ca8e16dda00c17b3605a1afe460348a45d7
FatSaw <eklesosek@gmail.com>
2024-02-10 00:11:53 +0200

    Create PacketPlayOutEntityHeadRotation.java

---
+++ src/me/bomb/decorativeentity/packet/PacketPlayOutEntityHeadRotation.java
@@ -0,0 +1,23 @@
+package me.bomb.decorativeentity.packet;
+
+import java.io.IOException;
+
+public class PacketPlayOutEntityHeadRotation extends Packet {
+	
+	public int id;
+	public byte yaw;
+	
+	public PacketPlayOutEntityHeadRotation(int id, byte yaw) {
+		super((byte) 54);
+		this.id = id;
+		this.yaw = yaw;
+	}
+
+	@Override
+	protected void write(PacketDataSerializer packetdataserializer) throws IOException {
+		super.write(packetdataserializer);
+		packetdataserializer.writeNum(id);
+		packetdataserializer.writeByte(yaw);
+	}
+
+}
