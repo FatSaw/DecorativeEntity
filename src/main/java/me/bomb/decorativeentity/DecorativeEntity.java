5aa560aa1fc75834967e28acd19f57e25ed35946
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-09-02 01:06:30 +0300

    Update DecorativeEntity.java

--- src/main/java/me/bomb/decorativeentity/DecorativeEntity.java
+++ src/main/java/me/bomb/decorativeentity/DecorativeEntity.java
@@ -4,11 +4,25 @@ import java.io.File;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DecorativeEntity extends JavaPlugin {
 	
+	private final PacketSender sender;
+	
+	public DecorativeEntity() {
+		this.sender = new PacketSender();
+	}
+	
 	public void onEnable() {
+		this.reload();
+		PluginCommand decorativeentitycommand = getCommand("decorativeentity");
+		decorativeentitycommand.setExecutor(new DeCommand(this));
+		Bukkit.getPluginManager().registerEvents(new EventListener(sender), this);
+	}
+	
+	protected void reload() {
 		File workingdirectory = this.getDataFolder();
 		if(!workingdirectory.exists()) {
 			workingdirectory.mkdirs();
@@ -17,7 +31,8 @@ public final class DecorativeEntity extends JavaPlugin {
 		MinecartOptions minecartoptions = new MinecartOptions(logger, new File(workingdirectory, "minecart.yml"));
 		ArmorstandOptions armorstandoptions = new ArmorstandOptions(logger, new File(workingdirectory, "armorstand.yml"));
 		HumanOptions humanoptions = new HumanOptions(logger, new File(workingdirectory, "human.yml"));
-		PacketSender sender = new PacketSender(minecartoptions, armorstandoptions, humanoptions);
-		Bukkit.getPluginManager().registerEvents(new EventListener(sender), this);
+		this.sender.minecartoptions = minecartoptions;
+		this.sender.armorstandoptions = armorstandoptions;
+		this.sender.humanoptions = humanoptions;
 	}
 }
